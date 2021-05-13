5efa0ba5184742279a526a0f46ec03f7749b6192
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-05-13 11:01:13 -0400

    created About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <div>
+                <h1>Pupster</h1>
+                <h3>They're Good Girls and Boys</h3>
+            </div>
+            <div>
+                <h3>Welcome to Pupster!</h3>
+                <p>
+                    Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
+                    mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
+                    porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
+                    semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
+                    rhoncus mollis diam, sit amet facilisis lectus blandit at. Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
+                    mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
+                    porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
+                    semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
+                    rhoncus mollis diam, sit amet facilisis lectus blandit at.
+
+                    Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
+                    mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
+                    porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
+                    semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
+                    rhoncus mollis diam, sit amet facilisis lectus blandit at.Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
+                    mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
+                    porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
+                    semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
+                    rhoncus mollis diam, sit amet facilisis lectus blandit at.Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
+                    mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
+                    porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
+                    semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
+                    rhoncus mollis diam, sit amet facilisis lectus blandit at.
+                    
+                </p>
+            </div>
+
+        </div>
+    );
+}
+
+export default About;
